647582b7c4b443da6bdb1ce5f1b5ebf782fd7f27
Sudhir <sudhirtumati@outlook.com>
2018-08-22 11:41:11 +0800

    Added another testcase to test object instantiation

---
+++ src/main/java/com/sudhirt/samples/tdd/PrimeFactors.java
@@ -0,0 +1,4 @@
+package com.sudhirt.samples.tdd;
+
+public class PrimeFactors {
+}
--- src/test/java/com/sudhir/samples/tdd/PrimeFactorsTest.java
+++ src/test/java/com/sudhir/samples/tdd/PrimeFactorsTest.java
@@ -1,11 +1,16 @@
 package com.sudhir.samples.tdd;
 
+import com.sudhirt.samples.tdd.PrimeFactors;
 import org.junit.jupiter.api.Test;
 
 public class PrimeFactorsTest {
 
     @Test
     public void nothing() {
+    }
 
+    @Test
+    public void createsInstance() {
+        PrimeFactors primeFactors = new PrimeFactors();
     }
 }
